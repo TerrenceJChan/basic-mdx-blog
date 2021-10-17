c968e2691a2168da65d38ca5daf823faa146ec79
Terrence Chan <terrencechan.writer@gmail.com>
2021-10-16 21:29:15 -0400

    Content card and button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import React from "react"
-import { Container, FeatureImage } from "../components"
+import { Container, FeatureImage, Content, ContentCard } from "../components"
 
 const IndexPage = () => {
   return (
     <Container>
       <FeatureImage />
+      <Content>
+        <ContentCard
+          date="October 16, 2021"
+          title="Our Content Card!"
+          excerpt="Slowly but surely, we're building our first Gatsby blog up from scratch. It's been a journey, but the end result will be well worth it."
+          slug="/our-content-card"
+        />
+      </Content>
     </Container>
   )
 }
