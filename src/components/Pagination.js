56fcf6fbefec5a174bb296c27e7d9b7ed7212018
Terrence Chan <terrencechan.writer@gmail.com>
2021-10-18 02:15:58 -0400

    Change text to 'Next'

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,7 +5,7 @@ export const Pagination = ({isFirst, isLast, prevPage, nextPage}) => {
     return (
         <PaginationWrapper isFirst={isFirst} isLast={isLast}>
             <PaginationElement to={prevPage}>Previous Page</PaginationElement>
-            <PaginationElement to={nextPage}>Previous Page</PaginationElement>
+            <PaginationElement to={nextPage}>Next Page</PaginationElement>
         </PaginationWrapper>
     )
 }
